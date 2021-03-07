6faa32bb5fb5e7f60c68f66812af979e5a6ef053
sayantabonny <sayantabonny@gmail.com>
2021-03-07 22:20:21 +0530

    Dynamic data- props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './App.css';
 import Person from './Person/Person'
+import {Person_Props} from './Person/Person'
 
 
 function App() {
   return (
     <div className="App">
       <h3>I am Sayanta</h3>
+      <Person/>
+      <Person/>
+      <Person/>
+      <Person/>
+      <h1>Props i.e Arguments</h1>
+      <Person_Props name="Sayanta" age="25"/>
+      <Person_Props name="Ranjan" age="26"/>
+      <Person_Props name="Tuhin"  age="23">Hobbies: Studying</Person_Props>
+      <Person_Props name="Rusha"  age="24"/>
     </div>
   );
   // or we could do return React.createElement('div',{className:'App'},React.createElement('h1',null,'I am Sayanta as above'));
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 
 const Person = () => {
-    return <p> I am from Person </p>
+    return <p> I am from Person who is {Math.floor(Math.random()*30)+1}</p>
 }
 
 
-export default Person;
+export const Person_Props = (any_name) => {
+    return <p> I am from {any_name.name} who is {any_name.age}</p>
+}
+
+export default  Person;
+
